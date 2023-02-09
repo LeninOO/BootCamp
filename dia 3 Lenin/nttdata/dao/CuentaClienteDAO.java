0e2da16f9f9083b45e465d43028281939fc30ded
LeninOO <107270453+LeninOO@users.noreply.github.com>
2023-02-09 14:56:02 -0500

    Add files via upload

---
+++ dia 3 Lenin/nttdata/dao/CuentaClienteDAO.java
@@ -0,0 +1,9 @@
+package com.nttdata.dao;
+
+import com.nttdata.domain.CuentaCliente;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class CuentaClienteDAO implements PanacheRepository<CuentaCliente> {
+
+}
