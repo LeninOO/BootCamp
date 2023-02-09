b1b129a7fc4a40e70efb239b2dd735c282fb2444
LeninOO <107270453+LeninOO@users.noreply.github.com>
2023-02-09 14:48:36 -0500

    Update CuentaServiceImpl.java

--- dia 3 Lenin/nttdata/services/CuentaServiceImpl.java
+++ dia 3 Lenin/nttdata/services/CuentaServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Objects;
 
 import com.nttdata.dao.TransaccionDAO;
-import com.nttdata.domain.Operaciones;
+import com.nttdata.domain.Cuenta;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -22,13 +22,16 @@ public class CuentaServiceImpl implements ICuentaService{
 	 */
 	@Override
 	@Transactional
-	public Operaciones registrar(Operaciones cuenta) throws Exception {
+	/**
+	 *  Registra  una  nueva  cuenta
+	 */
+	public Cuenta registrar(Cuenta cuenta) throws Exception {
 
 		if(Objects.nonNull(cuenta)) {
 
 			LocalDateTime fcActual = LocalDateTime.now();
 
-			pagocredito.setFcAltaFila(fcActual);
+			cuenta.setFcAltaFila(fcActual);
 			this.dao.persist(cuenta);
 
 		}
@@ -39,7 +42,10 @@ public class CuentaServiceImpl implements ICuentaService{
 	 *
 	 */
 	@Override
-	public Operaciones modificar(Operaciones cuenta) throws Exception {
+	/**
+	 actualizacion del ultimo movimiento de la cuenta
+	 */
+	public Cuenta modificar(Cuenta cuenta) throws Exception {
 
 		if(Objects.nonNull(cuenta)) {
 
@@ -53,22 +59,22 @@ public class CuentaServiceImpl implements ICuentaService{
 		return cuenta;
 	}
 	/**
-	 *
+	 * Obtener el listado de las cuentas que se maneja
 	 */
 	@Override
-	public List<Operaciones> listarCuenta() throws Exception {
+	public List<Cuenta> listarCuenta() throws Exception {
 
 		return this.dao.findAll().list();
 	}
-
+  
 
 	/**
-	 *
+	 *  Eliminar o cancelar una cuenta, este método no serainvocado por la interfaz
 	 */
 	@Override
 	public void eliminar(Integer id) throws Exception {
 
-		Operaciones cuenta = new cuenta();
+		Cuenta cuenta = new cuenta();
 
 		if(Objects.nonNull(id)) {
 			LocalDateTime fcBaja =  LocalDateTime.now();
@@ -80,10 +86,10 @@ public class CuentaServiceImpl implements ICuentaService{
 
 	}
 	/**
-	 *
+	 *    Busca de acuerdo al indice la cuenta correspondiente
 	 */
 	@Override
-	public Operaciones buscarPorId(Long id) throws Exception {
+	public Cuenta buscarPorId(Long id) throws Exception {
 
 		return this.dao.findById(id);
 	}
